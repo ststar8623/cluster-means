5f7433b4b8927ff5b7653666cbcc9d1286bb9c2a
ststar8623 <ststar8623@gmail.com>
2017-07-26 16:04:47 -0700

    change port

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.listen(3001, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log('Server is listening on port 3001');
 });
 
